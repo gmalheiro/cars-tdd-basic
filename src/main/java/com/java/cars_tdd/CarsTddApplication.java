1530f7436183798371ef57faeffbafe68a77237a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-14 19:27:45 -0300

    first commit add project to github

---
+++ src/main/java/com/java/cars_tdd/CarsTddApplication.java
@@ -0,0 +1,13 @@
+package com.java.cars_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/cars_tdd/CarsTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cars_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
